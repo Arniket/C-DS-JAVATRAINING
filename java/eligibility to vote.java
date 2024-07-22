9d6054995a3d10b2735203dbf6854c72b37bc31c
Arniket Rai <arniketrai@gmail.com>
2024-07-22 16:02:51 +0530

    code changed

--- java/eligibility to vote.java
+++ java/eligibility to vote.java
@@ -1,17 +1,11 @@
 import java.util.*;
 
 class ValidAge {
-public class void Main (string[] args){
-    Scanner sc = new Scanner (System.in);
-    System.out.println ("enter your age");
-    int age = sc.nextInt();
-    string eligible =(age>=18)? "eligible":"noteligible";
-    System.out.println (eligible);
-   }
-}
-
-)
-
-}
-
+    public static void Main(string[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = sc.nextInt();
+        string eligible = (age >= 18) ? "eligible" : "noteligible";
+        System.out.println(eligible);
+    }
 }
