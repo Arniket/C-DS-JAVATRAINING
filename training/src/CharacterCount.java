7755e800916f6558224d45b6ba8b92ba76f05c96
Arniket Rai <arniketrai@gmail.com>
2024-07-22 12:18:11 +0530

    character count in java

---
+++ training/src/CharacterCount.java
@@ -0,0 +1,15 @@
+public class CharacterCount {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int count = 0;
+
+        // Loop through the string and count each character
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') { // Optional: Exclude spaces from the count
+                count++;
+            }
+        }
+
+        System.out.println("Total number of characters: " + count);
+    }
+}
