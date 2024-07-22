24dfd3643585234fa43d03bcb5733a24d4596fa2
Arniket Rai <arniketrai@gmail.com>
2024-07-22 15:58:23 +0530

    eligibility to vote

---
+++ java/eligibility to vote.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class ValidAge {
+public class void Main (string[] args){
+    Scanner sc = new Scanner (System.in);
+    System.out.println ("enter your age");
+    int age = sc.nextInt();
+    string eligible =(age>=18)? "eligible":"noteligible";
+    System.out.println (eligible);
+   }
+}
+
+)
+
+}
+
+}
