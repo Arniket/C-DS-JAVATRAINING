0f59ee1b4502dbc615929372f1bc33248769d42e
Arniket Rai <arniketrai@gmail.com>
2024-07-24 15:22:19 +0530

    change code

--- java/tempCodeRunnerFile.java
+++ java/tempCodeRunnerFile.java
@@ -1,8 +1,11 @@
-
- class printaddnaturalnumber{
-    public static void main (String[] args) {
-        for (int i=1;i<=10;i++){
-        System.out.println (i);
-        }
+ class ContStatement {
+public static void main (String[] args){
+for (int  i=1 ; i<=10;i++) { 
+  
+    if (i==6){
+        continue;
     }
+    System.out.println(i);
+}
+}
 }
