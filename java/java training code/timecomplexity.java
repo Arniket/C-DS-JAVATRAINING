a816d1c69db05cc572bd2cd75b5b4eb743fc1959
Arniket Rai <arniketrai@gmail.com>
2024-07-27 21:08:06 +0530

    time complexity

---
+++ java/java training code/timecomplexity.java
@@ -0,0 +1,9 @@
+public class timecomplexity {
+    public static void main(String[] args) {
+        int i, n = 64;
+        for (i = 2; i <= n; i = (int) Math.pow(i, 2)) {
+            System.out.println("Hello World");
+        }
+    }
+
+}
