f46d7f1808acba412b5a8008be3e0ab1dc19411c
Arniket Rai <arniketrai@gmail.com>
2024-07-27 21:01:15 +0530

    Bmi Calculator

---
+++ java/java training code/conditionalstatement.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class conditionalstatement {
+    public static void main(String[] args) {
+        System.out.println("enter user age:");
+        Scanner sc = new Scanner(System.in);
+        int myAge = scanner.nextInt();
+        if (myAge > 18) {
+            System.out.println("eligible");
+        } else {
+            System.out.println("not valid");
+        }
+    }
+}
