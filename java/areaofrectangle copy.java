52e5cbce8f501b33015f5f4fad134052b7529fe9
Arniket Rai <arniketrai@gmail.com>
2024-07-29 13:53:01 +0530

    areaofrectangle

---
+++ java/areaofrectangle copy.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class areaofrectangle {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the length of the rectangle");
+            int length = sc.nextInt();
+            System.out.println("Enter the breadth of the rectangle");
+            int breadth = sc.nextInt();
+            areaofRectangle(length, breadth);
+            System.out.println("Area of rectangle is: " + (length * breadth));
+            sc.close();
+        }
+    }
+
+    private static int areaofRectangle(int length, int breadth) {
+        return (length * breadth);
+    }
+}
